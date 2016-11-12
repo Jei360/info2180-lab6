0de3032353c4cc7cafa9533f8c1f47260631574c
Jovan Dalhouse <jovan_360@hotmail.com>
2016-11-12 01:05:53 +0000

    Exercise 4 attemp 1

--- request.php
+++ request.php
@@ -3,7 +3,7 @@
 // accept a term (keyword)
 // respond with a value
 
-$query = $_GET['q'];
+/*$query = $_GET['q'];
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
@@ -16,4 +16,31 @@ $definition = [
 
 print "<h3>" . strtoupper($query) . "</h3>";
 print "<p>" . $definition[$query] . "</p>";
+
+
+*/
+$xmldata= '<?xml version="1.0" encoding="UTF-8"?> 
+<entries>     
+    <definition name="definition" author="John">
+                 a statement of the exact meaning of a word, especially in a dictionary.
+    </definition>
+    <definition name="bar" author="Mary">
+          a place that sells alcholic beverages
+    </definition>
+    <definition name="html" author="Kimberly">
+                 The standard markup language for creating web pages and web applications.
+    </definition>
+    <definition name="css" author="Gayle">
+                 A style sheet language used for describing the presentation of a document written in a markup language.
+    </definition>
+    <definition name="javascript" author="Monique">
+                 A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+    </definition>
+    <definition name="php" author="Daniel">
+                 A server-side scripting language, and a powerful tool for making dynamic and interactive websites
+    </definition>
+</entries>'; 
+header("Content-Type:text/xtml");
+$xmld= new SimpleXMLElement($xmldata);
+echo $xmld->asXML();
 ?>
